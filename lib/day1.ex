49babfce20e79d72150d2dec41641fb23dce82ee
Frédéric Boyer <frederic.boyer@amilia.com>
2021-12-03 20:48:49 +0000

    Fix a lil typo

--- lib/day1.ex
+++ lib/day1.ex
@@ -8,7 +8,7 @@ defmodule AdventOfCode2021.Day1 do
   def part1() do
     "./assets/day1.txt"
     |> read_measurements()
-    |> count_of_increased_measurements(0)
+    |> count_increased_measurements(0)
   end
 
   @spec read_measurements(String.t()) :: [integer]
@@ -19,34 +19,34 @@ defmodule AdventOfCode2021.Day1 do
     |> Enum.map(&String.to_integer/1)
   end
 
-  @spec count_of_increased_measurements([integer], integer) :: integer
-  def count_of_increased_measurements([_m1 | []], count), do: count
+  @spec count_increased_measurements([integer], integer) :: integer
+  def count_increased_measurements([_m1 | []], count), do: count
 
-  def count_of_increased_measurements([m1, m2 | rest], count) when m1 < m2 do
-    count_of_increased_measurements([m2 | rest], count + 1)
+  def count_increased_measurements([m1, m2 | rest], count) when m1 < m2 do
+    count_increased_measurements([m2 | rest], count + 1)
   end
 
-  def count_of_increased_measurements([_m1 | rest], count) do
-    count_of_increased_measurements(rest, count)
+  def count_increased_measurements([_m1 | rest], count) do
+    count_increased_measurements(rest, count)
   end
 
   @spec part2() :: integer
   def part2() do
     "./assets/day1.txt"
     |> read_measurements()
-    |> count_of_increased_sums(0)
+    |> count_increased_sums(0)
   end
 
-  @spec count_of_increased_sums([integer], integer) :: integer
-  def count_of_increased_sums([_m1 | []], count), do: count
-  def count_of_increased_sums([_m1, _m2 | []], count), do: count
-  def count_of_increased_sums([_m1, _m2, _m3 | []], count), do: count
+  @spec count_increased_sums([integer], integer) :: integer
+  def count_increased_sums([_m1 | []], count), do: count
+  def count_increased_sums([_m1, _m2 | []], count), do: count
+  def count_increased_sums([_m1, _m2, _m3 | []], count), do: count
 
-  def count_of_increased_sums([m1, m2, m3, m4 | rest], count) when m1 + m2 + m3 < m2 + m3 + m4 do
-    count_of_increased_sums([m2, m3, m4 | rest], count + 1)
+  def count_increased_sums([m1, m2, m3, m4 | rest], count) when m1 + m2 + m3 < m2 + m3 + m4 do
+    count_increased_sums([m2, m3, m4 | rest], count + 1)
   end
 
-  def count_of_increased_sums([_m1, m2, m3, m4 | rest], count) do
-    count_of_increased_sums([m2, m3, m4 | rest], count)
+  def count_increased_sums([_m1, m2, m3, m4 | rest], count) do
+    count_increased_sums([m2, m3, m4 | rest], count)
   end
 end
